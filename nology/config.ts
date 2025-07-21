6e9bfc9e9f9d54b5c4260ebf23e187b6c974c424
gibranadzhar <gbrankahlil2@gmail.com>
2025-07-21 14:46:29 +0700

    Create config.ts

---
+++ nology/config.ts
@@ -0,0 +1,13 @@
+export default {
+  ownerNumber: ["6283827963304"],
+  ownerName: "Brannz",
+  botName: "NologyBot",
+  language: "id",
+
+  mess: {
+    owner: "Fitur ini hanya untuk owner bot!",
+    wait: "⏳ Mohon tunggu sebentar...",
+    error: "❌ Terjadi kesalahan!",
+    done: "✅ Selesai!",
+  }
+};
